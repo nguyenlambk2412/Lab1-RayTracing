3ca2d56b64fd0266c3caa8b3d5d6fb7f237c3313
nguyenlambk2412 <nguyenlambk2412@outlook.com>
2022-11-12 20:56:41 +0100

    A1_Shadow

--- main.cpp
+++ main.cpp
@@ -53,17 +53,24 @@ void writeColor(int index, Vec3 p, uint8_t *pixels) {
 
 Color traceRay(const Ray &r, Scene scene, int depth) {
     Color c, directColor, reflectedColor, refractedColor;
-    if (depth < 0) return c;
-
+    
     Intersection hit, shadow;
     if (!scene.intersect(r, hit)) return Color(0.0f, 0.0f, 0.0f); // Background color
 
+    //light info
     const Vec3 lightPos(0.0f, 30.0f, -5.0f);
     Vec3 lightDir = lightPos - hit.position;
     lightDir.normalize();
 
-    directColor = Color(1.0f, 1.0f, 1.0f);
-
+    Ray shadowRay = hit.getShadowRay(lightPos);
+    if (!scene.intersect(shadowRay, shadow))
+    {
+        //No intersect between the shadowRay and objects
+        directColor = hit.material.color * (lightDir * hit.normal); // The 1st *: normal multiple, the 2nd *:dot product
+    } else {
+        //shadow ray intersects with an object
+        directColor = Color(0.0f, 0.0f, 0.0f);
+    }
     c = directColor;
 
     return c;
@@ -106,22 +113,22 @@ int main() {
     };
 
     // TODO: Uncomment to render floor triangles
-    // scene.push(Triangle(&vertices[0], whiteDiffuse)); // Floor 1
-    // scene.push(Triangle(&vertices[3], whiteDiffuse)); // Floor 2
+    scene.push(Triangle(&vertices[0], whiteDiffuse)); // Floor 1
+    scene.push(Triangle(&vertices[3], whiteDiffuse)); // Floor 2
 
     // TODO: Uncomment to render Cornell box
-    // scene.push(Triangle(&vertices[6], whiteDiffuse));  // Back wall 1
-    // scene.push(Triangle(&vertices[9], whiteDiffuse));  // Back wall 2
-    // scene.push(Triangle(&vertices[12], whiteDiffuse)); // Ceiling 1
-    // scene.push(Triangle(&vertices[15], whiteDiffuse)); // Ceiling 2
-    // scene.push(Triangle(&vertices[18], redDiffuse));   // Red wall 1
-    // scene.push(Triangle(&vertices[21], redDiffuse));   // Red wall 2
-    // scene.push(Triangle(&vertices[24], greenDiffuse)); // Green wall 1
-    // scene.push(Triangle(&vertices[27], greenDiffuse)); // Green wall 2
+     scene.push(Triangle(&vertices[6], whiteDiffuse));  // Back wall 1
+     scene.push(Triangle(&vertices[9], whiteDiffuse));  // Back wall 2
+     scene.push(Triangle(&vertices[12], whiteDiffuse)); // Ceiling 1
+     scene.push(Triangle(&vertices[15], whiteDiffuse)); // Ceiling 2
+     scene.push(Triangle(&vertices[18], redDiffuse));   // Red wall 1
+     scene.push(Triangle(&vertices[21], redDiffuse));   // Red wall 2
+     scene.push(Triangle(&vertices[24], greenDiffuse)); // Green wall 1
+     scene.push(Triangle(&vertices[27], greenDiffuse)); // Green wall 2
 
     // TODO: Uncomment to render reflective spheres
-    // scene.push(Sphere(Vec3(7.0f, 3.0f, 0.0f), 3.0f, yellowReflective));
-    // scene.push(Sphere(Vec3(9.0f, 10.0f, 0.0f), 3.0f, yellowReflective));
+     scene.push(Sphere(Vec3(7.0f, 3.0f, 0.0f), 3.0f, yellowReflective));
+     scene.push(Sphere(Vec3(9.0f, 10.0f, 0.0f), 3.0f, yellowReflective));
 
     // TODO: Uncomment to render refractive spheres
     // scene.push(Sphere(Vec3(-7.0f, 3.0f, 0.0f), 3.0f, transparent));
@@ -135,7 +142,7 @@ int main() {
     camera.setup(imageWidth, imageHeight);
 
     // Ray trace pixels
-    int depth = 3;
+    int depth = 2;
     std::cout << "Rendering... ";
     clock_t start = clock();
     for (int j = 0; j < imageHeight; ++j) {
--- swTriangle.cpp
+++ swTriangle.cpp
@@ -3,8 +3,46 @@
 namespace sw {
 
 bool Triangle::intersect(const Ray &r, Intersection &isect) const {
-    // TODO: Implement ray-triangle intersection
+    // 1st: Find the projection of rays origin onto the triangle plane:
+    Vec3 e1 = vertices[1] - vertices[0];    //Edge 1
+    Vec3 e2 = vertices[2] - vertices[0];    //Edge 2
 
+    Vec3 planeNormal = e1 % e2; // planeNormal by cross product
+    float m = -planeNormal * vertices[0]; // Plane equation: n.X + m = 0;
+    float t = (planeNormal * r.orig + m) / (-planeNormal * r.dir);  // plane intersection: t = (n.P+m)/(-n.d)
+    //Check t
+    if ((r.minT <= t) && (r.maxT >= t))
+    {
+        Vec3 Q = r.orig + t * r.dir; // projection point Q of P onto the triangle plane
+        Vec3 coVec = Q - vertices[0];    // vector r coplanar with e1, e2
+
+        //calculate barycentric v, w
+        float v = sqrt((e1 % coVec) * (e1 % coVec)) / sqrt(planeNormal * planeNormal);
+        float w = sqrt((coVec % e2) * (coVec % e2)) / sqrt(planeNormal * planeNormal);
+
+        if ((v >= 0) && (w >= 0) && ((v+w)<1))
+        {
+            isect.hitT = t;
+            isect.normal = planeNormal;
+            isect.normal.normalize();
+            isect.frontFacing = (-r.dir *isect.normal) > 0.0f;
+            if (!isect.frontFacing) isect.normal = -isect.normal;
+            isect.position = Q;
+            isect.material = material;
+            isect.ray = r;
+            return true;
+        } else
+            return false;
+
+    }
+    else
+        return false;
+    
+
+
+
+
+    
     return false;
 }
 
